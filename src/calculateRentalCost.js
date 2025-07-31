ff38c45d246986e46fa2300df52ad6fef1973ba1
Pavlo Mikitjuk <mikitjukpavlo@gmail.com>
2025-07-31 21:09:11 +0300

    add solution

--- src/calculateRentalCost.js
+++ src/calculateRentalCost.js
@@ -4,7 +4,14 @@
  * @return {number}
  */
 function calculateRentalCost(days) {
-  // write code here
+PRICE_PER_DAY = 40;
+  if (days < 3) {
+    return PRICE_PER_DAY * days;
+  } else if (days >= 3 && days <= 6) {
+    return PRICE_PER_DAY * days - 20; // basic discount
+  } else {
+    return PRICE_PER_DAY * days - 50; // additional discount for 7+ days
+  }
 }
 
 module.exports = calculateRentalCost;
